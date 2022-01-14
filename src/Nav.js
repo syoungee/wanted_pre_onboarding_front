473ed1b06aa7f2111a14392f10280270928b791b
syoungee <syoung_ee@naver.com>
2022-01-14 21:15:05 +0900

    마크업 완료

--- src/Nav.js
+++ src/Nav.js
@@ -177,27 +177,10 @@ function Nav() {
               />
             </svg>
           </li>
-          <div
-            style={{
-              display: "inline",
-              width: "32px",
-              hieght: "32px",
-              backgroundImage:
-                "url(https://lh3.googleusercontent.com/a/AATXAJx1gq5vdGSCZ9WdGUh2-CMrl9YT4A4Rc2o6tmF1=s96-c)",
-            }}
-          ></div>
+
           <li className="mdMoreVisible profileBox Aside_hasNewMywantedAlarm__p2Tfv">
             {/* 구글 아바타 */}
-            <div
-              className="avatarImage"
-              style={{
-                width: "32px",
-                hieght: "32px",
-                backgroundImage:
-                  "url(" +
-                  "https://lh3.googleusercontent.com/a/AATXAJx1gq5vdGSCZ9WdGUh2-CMrl9YT4A4Rc2o6tmF1=s96-c)",
-              }}
-            ></div>
+
             <button
               type="button"
               data-attribute-id="gnb"
@@ -207,40 +190,39 @@ function Nav() {
               data-badge="true"
             >
               <div className="avatarBorder">
-                <div className="avatarBorder">
-                  <div
-                    style={{
-                      width: "32px",
-                      height: "32px",
-                      backgroundImage: `url("https://lh3.googleusercontent.com/a/AATXAJx1gq5vdGSCZ9WdGUh2-CMrl9YT4A4Rc2o6tmF1=s96-c")`,
-                      backgroundRepeat: "no-repeat",
-                      backgroundPosition: "center",
-                      backgroundRepeat: "no-repeat",
-                      backgroundSize: "cover",
-                      display: "overflow",
-                    }}
-                  ></div>
-                </div>
+                <div
+                  className="avatarImage"
+                  style={{
+                    width: "32px",
+                    height: "32px",
+                    backgroundImage: `url("https://lh3.googleusercontent.com/a/AATXAJx1gq5vdGSCZ9WdGUh2-CMrl9YT4A4Rc2o6tmF1=s96-c")`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    display: "overflow",
+                  }}
+                ></div>
                 {/* <img src={UserName} style={{ width: "32px", hieght: "32px" }} /> */}
               </div>
-              <span
-                className="Badge_Badge__BJTzL"
-                style={{
-                  backgroundColor: "rgb(51, 102, 255)",
-                  width: "13px",
-                  height: "13px",
-                }}
-              >
-                <svg width="5" height="5" viewBox="0 0 6 6">
-                  <g fill="#fff" fillRule="nonzero">
-                    <path
-                      d="M6.647 11L6.647 7.259 6.688 7.259 9.158 11 11 11 11 5 9.353 5 9.353 8.357 9.322 8.357 7.089 5 5 5 5 11z"
-                      transform="translate(-123 -375) translate(20 365) translate(98 5)"
-                    ></path>
-                  </g>
-                </svg>
-              </span>
             </button>
+            <span
+              className="Badge_Badge__BJTzL"
+              style={{
+                backgroundColor: "rgb(51, 102, 255)",
+                width: "13px",
+                height: "13px",
+              }}
+            >
+              <svg width="5" height="5" viewBox="0 0 6 6">
+                <g fill="#fff" fillRule="nonzero">
+                  <path
+                    d="M6.647 11L6.647 7.259 6.688 7.259 9.158 11 11 11 11 5 9.353 5 9.353 8.357 9.322 8.357 7.089 5 5 5 5 11z"
+                    transform="translate(-123 -375) translate(20 365) translate(98 5)"
+                  ></path>
+                </g>
+              </svg>
+            </span>
           </li>
 
           <li
