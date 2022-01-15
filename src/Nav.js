78f02fe65b8f7e27c2bfe4c07aa16984ed72d623
syoungee <syoung_ee@naver.com>
2022-01-15 23:36:36 +0900

    nav bar 가로 완성

--- src/Nav.js
+++ src/Nav.js
@@ -4,21 +4,24 @@ import "./Nav.css";
 function Nav() {
   return (
     <div role="presentation" className="NavBar_className__y2qGg">
-      <div class="MainBar_MainBar__40DX7 isLoggedIn" role="presentation">
+      <div className="MainBar_MainBar__40DX7 isLoggedIn" role="presentation">
         <nav className="MainBar_MainBar_nav__kwHBF">
           <div className="MainBar_MainBar_nav_top__wXy52">
-            <button>
-              <img
-                src="https://static.wanted.co.kr/images/icon-menu.png"
-                alt="hamberger menu"
-                style={{
-                  width: "17px",
-                  height: "14px",
-                  objectFit: "contain",
-                }}
-              />
-              <i className="icon-logo_new"></i>
-            </button>
+            <div className="MainBar_MainBar_nav_top_logo__Y_Q5D">
+              <button>
+                <img
+                  className="MainBar_hamberger__yQfei"
+                  src="https://static.wanted.co.kr/images/icon-menu.png"
+                  alt="hamberger menu"
+                  style={{
+                    width: "17px",
+                    height: "14px",
+                    objectFit: "contain",
+                  }}
+                />
+                <i className="icon-logo_new"></i>
+              </button>
+            </div>
           </div>
           <ul className="Menu_className__gGcYQ">
             <li className="" data-attribute-id="gnb" data-gnb-kind="jobs">
@@ -94,34 +97,32 @@ function Nav() {
               data-attribute-id="gnb"
               data-gnb-kind="aiScore"
             >
-              <a>
-                AI 합격예측
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="11"
-                  viewBox="0 0 18 11"
+              <a>AI 합격예측</a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="11"
+                viewBox="0 0 18 11"
+              >
+                <g
+                  fill="none"
+                  fillRule="evenodd"
+                  fontFamily="AppleSDGothicNeo-SemiBold, Apple SD Gothic Neo"
+                  fontSize="9"
+                  fontWeight="500"
                 >
-                  <g
-                    fill="none"
-                    fillRule="evenodd"
-                    fontFamily="AppleSDGothicNeo-SemiBold, Apple SD Gothic Neo"
-                    fontSize="9"
-                    fontWeight="500"
-                  >
-                    <g fill="#36F">
+                  <g fill="#36F">
+                    <g>
                       <g>
                         <g>
                           <g>
                             <g>
                               <g>
-                                <g>
-                                  <text transform="translate(-931.000000, -13.000000) translate(224.000000, 7.000000) translate(210.000000, 6.000000) translate(350.000000, 0.000000) translate(147.000000, 0.000000)">
-                                    <tspan x="0" y="8">
-                                      Beta
-                                    </tspan>
-                                  </text>
-                                </g>
+                                <text transform="translate(-931.000000, -13.000000) translate(224.000000, 7.000000) translate(210.000000, 6.000000) translate(350.000000, 0.000000) translate(147.000000, 0.000000)">
+                                  <tspan x="0" y="8">
+                                    Beta
+                                  </tspan>
+                                </text>
                               </g>
                             </g>
                           </g>
@@ -129,56 +130,70 @@ function Nav() {
                       </g>
                     </g>
                   </g>
-                </svg>
-              </a>
+                </g>
+              </svg>
             </li>
           </ul>
           <aside className="Aside_className___e5Bi isLoggedIn">
             <ul>
               <li>
                 {/* 돋보기 */}
-                <svg
-                  xmlns="https://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
+                <button
+                  type="button"
+                  aria-label="searchButton"
+                  className="searchButton"
+                  data-attribute-id="gnb"
+                  data-gnb-kind="search"
                 >
-                  <path
-                    id="qt2dnsql4a"
-                    d="M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z"
-                  ></path>
-                  <g fill="#333" fillRule="evenodd">
-                    <g transform="translate(-1079 -16) translate(224 7) translate(855 9)">
-                      <mask id="1dencd96ob" fill="#333">
-                        <use></use>
-                      </mask>
-                      <use
-                        fillRule="nonzero"
-                        stroke="currentColor"
-                        strokeWidth=".3"
-                      ></use>
+                  <svg
+                    xmlns="https://www.w3.org/2000/svg"
+                    width="18"
+                    height="18"
+                    viewBox="0 0 18 18"
+                  >
+                    <path
+                      id="qt2dnsql4a"
+                      d="M15.727 17.273a.563.563 0 10.796-.796l-4.875-4.875-.19-.165a.563.563 0 00-.764.028 5.063 5.063 0 111.261-2.068.562.562 0 101.073.338 6.188 6.188 0 10-1.943 2.894l4.642 4.644z"
+                    ></path>
+                    <g fill="#333" fillRule="evenodd">
+                      <g transform="translate(-1079 -16) translate(224 7) translate(855 9)">
+                        <mask id="1dencd96ob" fill="#333">
+                          <use></use>
+                        </mask>
+                        <use
+                          fillRule="nonzero"
+                          stroke="currentColor"
+                          strokeWidth=".3"
+                        ></use>
+                      </g>
                     </g>
-                  </g>
-                </svg>
+                  </svg>
+                </button>
               </li>
 
               <li className="">
                 {/* 종 */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="13px"
-                  y="13px"
-                  width="18px"
-                  height="18px"
-                  viewBox="0 0 18 18"
+                <button
+                  aria-label="notiButton"
+                  className="notiButton"
+                  data-attribute-id="gnb"
+                  data-gnb-kind="bell"
                 >
-                  <path
-                    fill="#333"
-                    d="M7.554 14.813h3.183a1.689 1.689 0 01-3.183 0zm1.592 2.25a2.813 2.813 0 002.812-2.813.563.563 0 00-.562-.563h-7.5c-.31 0-.541-.014-.699-.04.018-.036.04-.077.066-.123.036-.065.354-.605.46-.8.477-.875.735-1.676.735-2.599V6.75c0-2.656 2.057-4.688 4.688-4.688 2.63 0 4.687 2.032 4.687 4.688v3.375c0 .923.258 1.724.736 2.6.106.194.424.734.46.799.026.046.047.087.065.123-.157.026-.389.04-.698.04a.564.564 0 000 1.126c1.263 0 1.896-.221 1.896-1.002 0-.26-.092-.494-.28-.833-.045-.083-.361-.619-.456-.792-.395-.724-.598-1.355-.598-2.061V6.75c0-3.28-2.563-5.813-5.812-5.813S3.333 3.47 3.333 6.75v3.375c0 .706-.203 1.337-.598 2.06-.094.174-.41.71-.456.793-.188.339-.279.572-.279.833 0 .78.632 1.002 1.896 1.002H6.39a2.813 2.813 0 002.756 2.25z"
-                  />
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="13px"
+                    y="13px"
+                    width="18px"
+                    height="18px"
+                    viewBox="0 0 18 18"
+                  >
+                    <path
+                      fill="#333"
+                      d="M7.554 14.813h3.183a1.689 1.689 0 01-3.183 0zm1.592 2.25a2.813 2.813 0 002.812-2.813.563.563 0 00-.562-.563h-7.5c-.31 0-.541-.014-.699-.04.018-.036.04-.077.066-.123.036-.065.354-.605.46-.8.477-.875.735-1.676.735-2.599V6.75c0-2.656 2.057-4.688 4.688-4.688 2.63 0 4.687 2.032 4.687 4.688v3.375c0 .923.258 1.724.736 2.6.106.194.424.734.46.799.026.046.047.087.065.123-.157.026-.389.04-.698.04a.564.564 0 000 1.126c1.263 0 1.896-.221 1.896-1.002 0-.26-.092-.494-.28-.833-.045-.083-.361-.619-.456-.792-.395-.724-.598-1.355-.598-2.061V6.75c0-3.28-2.563-5.813-5.812-5.813S3.333 3.47 3.333 6.75v3.375c0 .706-.203 1.337-.598 2.06-.094.174-.41.71-.456.793-.188.339-.279.572-.279.833 0 .78.632 1.002 1.896 1.002H6.39a2.813 2.813 0 002.756 2.25z"
+                    />
+                  </svg>
+                </button>
               </li>
-
               <li className="mdMoreVisible profileBox Aside_hasNewMywantedAlarm__p2Tfv">
                 {/* 구글 아바타 */}
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Nav from "./Nav.js";
+import App from "./App.js";
 
 ReactDOM.render(<Nav />, document.getElementById("root"));
