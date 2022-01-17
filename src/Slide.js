a436af96abca195d8e0b1186fbeec6c1ec611069
syoungee <syoung_ee@naver.com>
2022-01-17 10:19:15 +0900

    중간 미디어에서 이미지 넣기

---
+++ src/Slide.js
@@ -0,0 +1,159 @@
+import React from "react";
+import "./Slide.css";
+
+function Slide() {
+  return (
+    <div className="TopBanner_TopBanner__yxapE">
+      <div className="slick-slider slick-initialized" dir="ltr">
+        <div className="slick-list" style={{ padding: "0px 40px" }}>
+          <div
+            className="slick-track"
+            style={{
+              opacity: "1",
+              width: "37395",
+            }}
+          >
+            <div className="slick-slide slick-cloned" style={{ width: "1060" }}>
+              <div>
+                {/*                   
+                <div
+                  data-index="-9"
+                  data-attribute-id="home__mainBanner__click"
+                  data-landing-uri="/events/pre_onboarding_course_6"
+                  data-link-kind="EVENT_DETAIL"
+                  data-content-title="개발자 되고싶은 분들!?"
+                  tabindex="-1"
+                  style={{ width: "1060", display: "inline-block" }}
+                >
+                  <div className="Image_Image__T6WBp">
+                    <a
+                      href="/events/pre_onboarding_course_6"
+                      className=""
+                      aria-label=""
+                    >
+                      <img
+                        src="https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg"
+                        alt="개발자 되고싶은 분들!?"
+                        className="Image_Image__image__Y2rLN"
+                      />
+                    </a>
+                  </div>
+                  <div className="Information_Information__SwERN">
+                    <h2>개발자 되고싶은 분들!?</h2>
+                    <h3>프론트엔드 무료 교육과정 참여하기</h3>
+                    <hr className="Divider_Divider__root__f2LD0 Information_Information__divider__Z8vel" />
+                    <a
+                      href="/events/pre_onboarding_course_6"
+                      className="Button_Button__root__V1ie3 Button_Button__text__GCOTx Button_Button__textPrimary__hcFzK Button_Button__sizeMedium__k0A1w Information_Information__directButton__8Kgp3"
+                      aria-label=""
+                    >
+                      <span className="Button_Button__label__1Kk0v">
+                        바로가기
+                        <span className="Button_Button__endIcon__MpDfc">
+                          <span className="SvgIcon_SvgIcon__root__8vwon">
+                            <svg
+                              className="SvgIcon_SvgIcon__root__svg__DKYBi"
+                              viewBox="0 0 18 18"
+                            >
+                              <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+                            </svg>
+                          </span>
+                        </span>
+                      </span>
+                    </a>
+                  </div>
+                </div> */}
+
+                <div
+                  data-index="2"
+                  className="slick-slide slick-active slick-center slick-current"
+                  tabindex="-1"
+                  aria-hidden="false"
+                  style={{ outline: "none", width: "1060px" }}
+                >
+                  <div>
+                    <div
+                      data-attribute-id="home__mainBanner__click"
+                      data-landing-uri="/events/hrclass220106"
+                      data-link-kind="EVENT_DETAIL"
+                      data-content-title="2022년 달라지는 노동법령"
+                      tabindex="-1"
+                      style={{ width: "100%", display: "inline-block" }}
+                    >
+                      <div className="Image_Image__T6WBp Image_Image__active___6EY5">
+                        <a
+                          href="/events/hrclass220106"
+                          className=""
+                          aria-label=""
+                        >
+                          <img
+                            src="https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg"
+                            alt="2022년 달라지는 노동법령"
+                            className="Image_Image__image__Y2rLN"
+                          />
+                        </a>
+                      </div>
+                      <div className="Information_Information__SwERN Information_Information__active__5qVDq">
+                        <h2>2022년 달라지는 노동법령</h2>
+                        <h3>노무관리 쟁점 한 눈에 파악하기</h3>
+                        <hr className="Divider_Divider__root__f2LD0 Information_Information__divider__Z8vel" />
+                        <a
+                          href="/events/hrclass220106"
+                          className="Button_Button__root__V1ie3 Button_Button__text__GCOTx Button_Button__textPrimary__hcFzK Button_Button__sizeMedium__k0A1w Information_Information__directButton__8Kgp3"
+                          aria-label=""
+                        >
+                          <span className="Button_Button__label__1Kk0v">
+                            바로가기
+                            <span className="Button_Button__endIcon__MpDfc">
+                              <span className="SvgIcon_SvgIcon__root__8vwon">
+                                <svg
+                                  className="SvgIcon_SvgIcon__root__svg__DKYBi"
+                                  viewBox="0 0 18 18"
+                                >
+                                  <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+                                </svg>
+                              </span>
+                            </span>
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        type="button"
+        className="TopBanner_arrow__BN_7d TopBanner_nextArrow__6wqx0"
+        aria-label="nextArrowButton"
+      >
+        <span className="SvgIcon_SvgIcon__root__8vwon">
+          <svg
+            className="SvgIcon_SvgIcon__root__svg__DKYBi"
+            viewBox="0 0 18 18"
+          >
+            <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+          </svg>
+        </span>
+      </button>
+      <button
+        type="button"
+        className="TopBanner_arrow__BN_7d TopBanner_prevArrow__B7nqk"
+        aria-label="prevArrowButton"
+      >
+        <span className="SvgIcon_SvgIcon__root__8vwon">
+          <svg
+            className="SvgIcon_SvgIcon__root__svg__DKYBi"
+            viewBox="0 0 18 18"
+          >
+            <path d="m6.045 9 5.978-5.977a.563.563 0 1 0-.796-.796L4.852 8.602a.562.562 0 0 0 0 .796l6.375 6.375a.563.563 0 0 0 .796-.796L6.045 9z"></path>
+          </svg>
+        </span>
+      </button>
+    </div>
+  );
+}
+export default Slide;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Nav from "./Nav.js";
-import App from "./App.js";
+import Slide from "./Slide.js";
+// import Carousel from "./Carousel.js";
 
-ReactDOM.render(<Nav />, document.getElementById("root"));
+ReactDOM.render(<Nav />, document.getElementById("navbar"));
+// ReactDOM.render(<Carousel />, document.getElementById("slide"));
+ReactDOM.render(<Slide />, document.getElementById("slide"));
