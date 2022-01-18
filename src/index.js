ff294afbc7de8fff0a0b64c28d554bc186ef9786
syoungee <syoung_ee@naver.com>
2022-01-18 12:06:36 +0900

    불필요한 코드 제거

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Nav from "./Nav.js";
 import Slide from "./Slide.js";
-// import Carousel from "./Carousel.js";
 
 ReactDOM.render(<Nav />, document.getElementById("navbar"));
-// ReactDOM.render(<Carousel />, document.getElementById("slide"));
 ReactDOM.render(<Slide />, document.getElementById("slide"));
